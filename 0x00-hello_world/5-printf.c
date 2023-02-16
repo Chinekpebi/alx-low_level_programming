77a0efd631aeab3d33b0b8b6fa7acf94ae7bc99b
chinekpebi <luckychiwekpebi@gmail.com>
2023-02-16 05:36:55 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * main - Printf function is used to print a line in this program
+ * Return: (0) (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0); }	
