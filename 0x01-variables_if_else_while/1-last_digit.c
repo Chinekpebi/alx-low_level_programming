b79b2edd9ea44565ca4efc0d987a615556f99bc8
chinekpebi <luckychiwekpebi@gmail.com>
2023-02-18 08:02:03 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,6 +19,6 @@ int main(void)
 	else if (last == 0)
 		print("last digit of %i is %i and is 0\n", n, last);
 	else if (last < 6)
-		print("last digit of %i is %i and is less than 6 and not0\n", n, last);
+		print("last digit of %i is %i and is less than 6 and not 0\n", n, last);
 	return (0);
 }
